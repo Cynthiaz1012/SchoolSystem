5bc13a718b571c85f9aca5250fe591ad08adabca
Cynthia <ouocwouo@gmail.com>
2025-12-06 15:34:14 -0500

    Add all argument Constructor

--- src/main/java/org/cynthia/Address.java
+++ src/main/java/org/cynthia/Address.java
@@ -16,6 +16,22 @@ public class Address {
     private Province province;
     private String postalCode;
 
+    public Address(int streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode.toUpperCase();
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+        }
+    }
+
     /**
      * Checks whether the given postal code is valid.
      * @param postalCode the postal code to check
