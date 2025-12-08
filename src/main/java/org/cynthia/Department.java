5afaa9b58963fac502c0af85bf470d9530c8b960
Cynthia <ouocwouo@gmail.com>
2025-12-07 20:55:52 -0500

    add isDepartmentNameValid method and change the id field

--- src/main/java/org/cynthia/Department.java
+++ src/main/java/org/cynthia/Department.java
@@ -2,18 +2,16 @@ package org.cynthia;
 
 
 public class Department {
-    private final String id;
     private String departmentId;
     private String departmentName;
-
-    public static int nextId = 1;
+    private static int nextId = 1;
 
     /**
      * generate the id for the next department
      * @return the id for the next department
      */
     private static String generateId(){
-        return String.format("%02d", nextId++);
+        return String.format("D%02d", nextId++);
     }
 
     /**
