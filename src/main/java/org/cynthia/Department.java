b2eb9fa655283c4647f96ba2851687128d64c2a3
Cynthia <ouocwouo@gmail.com>
2025-12-07 20:35:35 -0500

    add isDepartmentNameValid method

--- src/main/java/org/cynthia/Department.java
+++ src/main/java/org/cynthia/Department.java
@@ -16,4 +16,22 @@ public class Department {
         return String.format("%02d", nextId++);
     }
 
+    /**
+     * Checks whether a department name is valid.
+     * @param departmentName the department name to check
+     * @return true if valid, false otherwise
+     */
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+        for (int i = 0; i < departmentName.length(); i++) {
+            char c = departmentName.charAt(i);
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
