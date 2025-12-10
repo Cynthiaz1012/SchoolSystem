fb4da9dfb10149b1ca1fbd172ae959ad2f160874
Cynthia <ouocwouo@gmail.com>
2025-12-10 00:12:29 -0500

    add constructor in student class

--- src/main/java/org/cynthia/Student.java
+++ src/main/java/org/cynthia/Student.java
@@ -4,6 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @EqualsAndHashCode
 @Getter
 @Setter
@@ -22,6 +24,15 @@ public class Student {
      */
     private static String generateId(){
         return String.format("S%06d", nextId++);
+    }
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId++);
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new ArrayList<>();
     }
 
     public enum Gender {
