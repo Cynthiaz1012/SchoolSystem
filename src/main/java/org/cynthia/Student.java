9ea8bd27b3d0178bd8a6e1b0628cdc9320c7b04f
Cynthia <ouocwouo@gmail.com>
2025-12-09 22:49:17 -0500

    add enum Gender and generateId method

--- src/main/java/org/cynthia/Student.java
+++ src/main/java/org/cynthia/Student.java
@@ -15,4 +15,16 @@ public class Student {
     private Department department;
     private List<Course> registeredCourses;
     private static int nextId = 1;
+
+    /**
+     * generate the id for the next department
+     * @return the id for the next department
+     */
+    private static String generateId(){
+        return String.format("S%06d", nextId++);
+    }
+
+    public enum Gender {
+        MALE, FEMALE
+    }
 }
