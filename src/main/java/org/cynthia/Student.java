1f4952564e844434eb9bb806f6cb85ca1e8d0425
Cynthia <ouocwouo@gmail.com>
2025-12-09 22:00:05 -0500

    add student class's fields

--- src/main/java/org/cynthia/Student.java
+++ src/main/java/org/cynthia/Student.java
@@ -1,4 +1,11 @@
 package org.cynthia;
 
 public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private List<Course> registeredCourses;
+    private static int nextId = 1;
 }
