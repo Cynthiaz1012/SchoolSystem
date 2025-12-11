4ed798626211993265543568c30cc64bf879b8e5
Cynthia <ouocwouo@gmail.com>
2025-12-11 00:18:55 -0500

    add registerCourse method in student class

--- src/main/java/org/cynthia/Student.java
+++ src/main/java/org/cynthia/Student.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @EqualsAndHashCode
 @Getter
@@ -33,6 +34,31 @@ public class Student {
         this.address = address;
         this.department = department;
         this.registeredCourses = new ArrayList<>();
+    }
+
+    /**
+     * Registers this student for a given course.
+     * @param course the course to register
+     * @return  true if registration succeeded; false if already registered
+     */
+    public boolean registerCourse(Course course) {
+        if (course == null) return false;
+
+        if (registeredCourses.contains(course)) {
+            return false; // already registered
+        }
+
+        registeredCourses.add(course);
+
+        if (!course.getRegisteredStudents().contains(this)) {
+            course.getRegisteredStudents().add(this);
+        }
+
+        for (Assignment a : course.getAssignments()) {
+            a.getScores().add(null);
+        }
+
+        return true;
     }
 
     public enum Gender {
