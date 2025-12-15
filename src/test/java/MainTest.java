16efe9b8da1b7bf4e19a98459192e7e56dac9353
Cynthia <ouocwouo@gmail.com>
2025-12-15 00:03:37 -0500

    test registerCourse method

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,7 @@
 import org.cynthia.Address;
+import org.cynthia.Course;
 import org.cynthia.Department;
+import org.cynthia.Student;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -74,6 +76,43 @@ public class MainTest {
         String departmentName = "12345";
         boolean expected = false;
         boolean actual = Department.isDepartmentNameValid(departmentName);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("normal -> true")
+    void testRegisterCourse1() {
+        Student student = new Student("Jane Doe", Student.Gender.FEMALE,
+                new Address(11, "Terry", "Montreal", Address.Province.QC, "H3E1L4"),
+                new Department("Computer Science"));
+        Course course = new Course("Programming", 3.0, new Department("Computer Science"));
+        boolean expected = true;
+        boolean actual = student.registerCourse(course);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("register again -> false")
+    void testRegisterCourse2() {
+        Student student = new Student("Jane Doe", Student.Gender.FEMALE,
+                new Address(11, "Terry", "Montreal", Address.Province.QC, "H3E1L4"),
+                new Department("Computer Science"));
+        Course course = new Course("Programming", 3.0, new Department("Computer Science"));
+        student.registerCourse(course);
+        boolean expected = false;
+        boolean actual = student.registerCourse(course);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("course = null -> false")
+    void testRegisterCourse3() {
+        Student student = new Student("Jane Doe", Student.Gender.FEMALE,
+                new Address(11, "Terry", "Montreal", Address.Province.QC, "H3E1L4"),
+                new Department("Computer Science"));
+        Course course = null;
+        boolean expected = false;
+        boolean actual = student.registerCourse(course);
         Assertions.assertEquals(expected, actual);
     }
 }
